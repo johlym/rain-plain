21909ef42f78d3e7455567da235ed6bef7c9f35e
Johnathan Lyman <johnathan.lyman@heroku.com>
2020-02-27 17:22:15 -0800

    add logic

--- test.php
+++ test.php
@@ -1,13 +1,20 @@
 <?php
 $url = getenv('DATABASE_URL');
 
-var_dump(parse_url($url));
-var_dump(parse_url($url, PHP_URL_SCHEME));
-var_dump(parse_url($url, PHP_URL_USER));
-var_dump(parse_url($url, PHP_URL_PASS));
-var_dump(parse_url($url, PHP_URL_HOST));
-var_dump(parse_url($url, PHP_URL_PORT));
-var_dump(parse_url($url, PHP_URL_PATH));
-var_dump(parse_url($url, PHP_URL_QUERY));
-var_dump(parse_url($url, PHP_URL_FRAGMENT));
+if (getenv('DATABASE_URL')) {
+    echo "DATABASE_URL FOUND";
+    var_dump(parse_url($url));
+    var_dump(parse_url($url, PHP_URL_SCHEME));
+    var_dump(parse_url($url, PHP_URL_USER));
+    var_dump(parse_url($url, PHP_URL_PASS));
+    var_dump(parse_url($url, PHP_URL_HOST));
+    var_dump(parse_url($url, PHP_URL_PORT));
+    var_dump(parse_url($url, PHP_URL_PATH));
+    var_dump(parse_url($url, PHP_URL_QUERY));
+    var_dump(parse_url($url, PHP_URL_FRAGMENT));
+} 
+else {
+    echo "DATABASE_URL NOT FOUND.";
+}
+
 ?>

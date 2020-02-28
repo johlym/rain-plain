38a61fe7773ffd7165e6144cde454f96d7cc058a
Johnathan Lyman <johnathan.lyman@heroku.com>
2020-02-27 17:18:29 -0800

    test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$url = getenv('DATABASE_URL');
+
+var_dump(parse_url($url));
+var_dump(parse_url($url, PHP_URL_SCHEME));
+var_dump(parse_url($url, PHP_URL_USER));
+var_dump(parse_url($url, PHP_URL_PASS));
+var_dump(parse_url($url, PHP_URL_HOST));
+var_dump(parse_url($url, PHP_URL_PORT));
+var_dump(parse_url($url, PHP_URL_PATH));
+var_dump(parse_url($url, PHP_URL_QUERY));
+var_dump(parse_url($url, PHP_URL_FRAGMENT));
+?>
